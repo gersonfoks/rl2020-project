9b296ff091e8a3f6ebeebecf324c5245e03fcb97
Roboraw <s.poelmann@xs4all.nl>
2020-10-07 20:00:33 +0200

    Nchain

--- scripts/Nchain.py
+++ scripts/Nchain.py
@@ -6,16 +6,18 @@ from policies import *
 import gym
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from gym.envs.registration import register
 
-###
-n = 6
+n = 7
 
 Q = np.zeros((n, 2))
 
 for state in range(n):
-  Q[state,  1] = 1
+    Q[state,  0] = 1
 
-env = gym.make('NChain-v0')(n)
+register( id='NChainN-v0', entry_point='gym.envs.toy_text:NChainEnv', kwargs={'n': n} )
+env = gym.make('NChainN-v0')
+env.n = n
 print(env.action_space)
 print(Q)
 
@@ -28,10 +30,9 @@ for episode in range(3):
     trajectory_data = sample_episode(env, behavior_policy)
     print("Episode {}:\nStates {}\nActions {}\nRewards {}\nDones {}\n".format(episode,*trajectory_data))
 
-# np.random.seed(42)
-# V_10k = mc_ordinary_importance_sampling(env, nchain_policy, policy, 10000, sample_episode)
-# V_500k = mc_ordinary_importance_sampling(env, nchain_policy, policy, 500000, sample_episode)
-
+np.random.seed(42)
+V_10k = mc_weighted_importance_sampling(env,behavior_policy , target_policy, 10000, sample_episode)
+# V_500k = mc_weighted_importance_sampling(env, behavior_policy, target_policy, 500000, sample_episode)
 
 # Vs = [V_10k, V_500k]
 # x = np.arange(n)
@@ -46,10 +47,6 @@ for episode in range(3):
 
 
 
-### First we run the env with random agent
 
-np.random.seed(42)
-#V_10k = mc_weighted_importance_sampling(env,behavior_policy , target_policy, 10000, sample_episode)
-V_500k = mc_weighted_importance_sampling(env, behavior_policy, target_policy, 500000, sample_episode)
 
-print(V_500k)
+print(V_10k)
